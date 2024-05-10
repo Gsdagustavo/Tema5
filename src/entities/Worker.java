88ee37139da2d1fa2c656972816e655e4f59612b
Gustavo Daniel <gugadanielalvez@gmail.com>
2024-05-10 18:54:01 -0300

    Initialized the Worker class variables, getters, setters, constructors and toString

--- src/entities/Worker.java
+++ src/entities/Worker.java
@@ -1,4 +1,54 @@
 package entities;
 
 public class Worker {
+
+    private String cpf, name;
+    private int hoursWorked;
+    private char turn, category;
+    private float incomePerHour;
+
+    public Worker(String cpf, String name, int hoursWorked, char turn, char category, float incomePerHour) {
+        this.cpf = cpf;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.turn = turn;
+        this.category = category;
+        this.incomePerHour = incomePerHour;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public char getTurn() {
+        return turn;
+    }
+
+    public char getCategory() {
+        return category;
+    }
+
+    public float getIncomePerHour() {
+        return incomePerHour;
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "cpf='" + cpf + '\'' +
+                ", name='" + name + '\'' +
+                ", hoursWorked=" + hoursWorked +
+                ", turn=" + turn +
+                ", category=" + category +
+                ", incomePerHour=" + incomePerHour +
+                '}';
+    }
 }
