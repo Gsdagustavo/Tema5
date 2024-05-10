ea527e3fc165f9418c0130fec8113685323cfe45
Gustavo Daniel <gugadanielalvez@gmail.com>
2024-05-10 19:08:31 -0300

    Added methods for getting input (char, int, float)

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -13,4 +14,37 @@ public class Main {
 
     }
 
+    // Get Char Input
+    public static char getCharInput() {
+        char input = ' ';
+
+        try {
+            return sc.nextLine().charAt(0);
+        } catch (Exception e) {
+            System.err.println("An error has occurred.");
+            return input;
+        }
+    }
+
+    // Get Int input
+    public static int getIntInput() {
+        int input;
+
+        try {
+            return Integer.parseInt(sc.nextLine());
+        } catch (InputMismatchException | NumberFormatException e) {
+            return -1;
+        }
+    }
+
+    // Get Float input
+    public static float getFloatInput() {
+        float input = 0F;
+
+        try {
+            return Float.parseFloat(sc.nextLine());
+        } catch (InputMismatchException | NumberFormatException e) {
+            return -1;
+        }
+    }
 }
