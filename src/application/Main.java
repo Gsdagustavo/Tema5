1bfb3ab162f17241db4b1a5a30bb4fa872a66de1
Gustavo Daniel <gugadanielalvez@gmail.com>
2024-05-10 18:49:31 -0300

    Added the Main and Worker classes; initialized the Scanner

---
+++ src/application/Main.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.util.Scanner;
+
+public class Main {
+
+    // Scanner
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
